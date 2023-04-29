5907175e70e661626e638580f62c91e8f3c43c71
ppinng <thiti.saelee@mail.kmutt.ac.th>
2023-04-29 17:44:38 +0700

    Set up Firebase

---
+++ lib/screens/login_page/login_page.dart
@@ -0,0 +1,7 @@
+// Add GoRouter configuration outside the App class
+// import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+// import 'package:flutter/material.dart';
+// import 'package:go_router/go_router.dart';
+
+// import '../Home_page/home_page.dart';
+
