922b1c50f3191636b360efa2a1b739efbc4b289e
Bonk-er <patthanan156@gmail.com>
2023-05-03 15:00:45 +0700

    appbar

--- lib/screens/forgotpassword_page/forgot_page.dart
+++ lib/screens/forgotpassword_page/forgot_page.dart
@@ -129,6 +129,8 @@ class _forgotEmailState extends State<forgotEmail> {
                     onPressed: () {
                       // if (_formKey.currentState!.validate()) {
                       //   // showDialog(
+
+                      //   // showDialog(
                       //   //     context: context,
                       //   //     builder: (context) {
                       //   //       return AlertDialog(
@@ -308,8 +310,8 @@ class _forgotPasswordState extends State<forgotPassword> {
                   //   //       );
                   //   //     });
                   // }
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => const WelcomeScreen()));
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) => test()));
                 },
                 child: const Text(
                   "Next",
@@ -319,6 +321,154 @@ class _forgotPasswordState extends State<forgotPassword> {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+String? validateEmail(String? value) {
+  String pattern =
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+  RegExp regex = RegExp(pattern);
+  if (!regex.hasMatch(value ?? '')) {
+    return 'Enter Valid Email';
+  } else {
+    return null;
+  }
+}
+
+class test extends StatelessWidget {
+  test({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kBackgroundColor,
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.black, size: 38),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            color: kBackgroundColor,
+          ),
+        ),
+        title: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+          child: SearchField(),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Image.asset("images/forgot.png"),
+          ),
+        ],
+      ),
+      drawer: NavigationDrawer(
+        backgroundColor: kBackgroundColor,
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(30, 55, 0, 0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 100,
+                  width: 100,
+                  child: Image.asset("images/forgot.png"),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+                  child: Row(
+                    children: [
+                      const FaIcon(
+                        FontAwesomeIcons.user,
+                        size: 44,
+                        color: kBoxColor,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 20),
+                        child: TextButton(
+                          onPressed: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => const WelcomeScreen()));
+                          },
+                          child: const Text(
+                            'Profile',
+                            style: TextStyle(
+                              color: kBoxColor,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  children: [
+                    const FaIcon(
+                      FontAwesomeIcons.arrowRightFromBracket,
+                      size: 44,
+                      color: kBoxColor,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 20),
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => const WelcomeScreen()));
+                        },
+                        child: const Text(
+                          'Log out',
+                          style: TextStyle(
+                            color: kBoxColor,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+      body: Container(),
+    );
+  }
+}
+
+class SearchField extends StatelessWidget {
+  SearchField({super.key});
+  final _formKey = GlobalKey<FormState>();
+  final _searchController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 45,
+      child: TextField(
+        controller: _searchController,
+        style: const TextStyle(color: Colors.black),
+        cursorColor: Colors.black,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          prefixIcon: const Padding(
+            padding: EdgeInsets.fromLTRB(10, 10, 0, 0),
+            child: FaIcon(
+              FontAwesomeIcons.magnifyingGlass,
+              color: Colors.grey,
+            ),
+          ),
+          // border: InputBorder.none,
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(width: 2, color: Colors.grey),
+            borderRadius: BorderRadius.circular(15),
+          ),
+        ),
+        onChanged: (value) {
+          // Perform search functionality here
+        },
       ),
     );
   }
