e7af249711e14f25121fc378354719c4cf1176d4
mmmmay <premikamay@gmail.com>
2023-05-04 06:05:46 +0700

    [ADD] Widget in History and Favorite page

--- lib/screens/pages/history.dart
+++ lib/screens/pages/history.dart
@@ -4,6 +4,21 @@ import 'package:flutter/material.dart';
 import 'package:araigordaiwithme/constant.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+class Menu {
+  final String name;
+  final String image;
+  final int calories;
+  final double price;
+  final String location;
+
+  Menu(
+      {required this.name,
+      required this.image,
+      required this.calories,
+      required this.price,
+      required this.location});
+}
+
 class History extends StatefulWidget {
   const History({super.key});
 
@@ -12,6 +27,42 @@ class History extends StatefulWidget {
 }
 
 class _HistoryState extends State<History> {
+  final List<Menu> menus = [
+    Menu(
+        name: 'Spicy fried chicken with basil leaves',
+        image: 'images/krapao.png',
+        calories: 250,
+        price: 10.99,
+        location: 'New York'),
+    Menu(
+        name: 'Spicy Chicken Wings',
+        image: 'images/wingsaps.png',
+        calories: 350,
+        price: 14.99,
+        location: 'Los Angeles'),
+    Menu(
+        name: 'Grilled Chicken',
+        image: 'images/kaiyang.png',
+        calories: 450,
+        price: 19.99,
+        location: 'Chicago'),
+    Menu(
+        name: 'Chicken rice ',
+        image: 'images/kaowmungai.png',
+        calories: 550,
+        price: 24.99,
+        location: 'Houston'),
+    Menu(
+        name: 'Papaya Salad',
+        image: 'images/papayasalad.png',
+        calories: 650,
+        price: 29.99,
+        location: 'Miami'),
+  ];
+
+  // FoodList({super.key});
+  bool isFavorite = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -114,7 +165,127 @@ class _HistoryState extends State<History> {
         ],
       ),
       backgroundColor: kBackgroundColor,
-      body: Container(child: Text("test")),
+      body: ListView.builder(
+              itemCount: menus.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Column(
+                  children: <Widget>[
+                    Container(
+                      width: 348,
+                      height: 100,
+                      margin: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: Stack(
+                        children: [
+                          Row(
+                            //Food image
+                            children: <Widget>[
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(left: 20, right: 20),
+                                child: Image.asset(
+                                  menus[index].image,
+                                  height: 75,
+                                  width: 100,
+                                  colorBlendMode: BlendMode.darken,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              //Food Name
+                              Expanded(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      menus[index].name,
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 5,
+                                    ),
+                                    //Food Calories
+                                    Text(
+                                      'Calories: ${menus[index].calories}',
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                        color: Colors.grey,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                          //Grey Box bottom right
+                          const FavoriteButton(),
+                        ],
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+    );
+  }
+}
+
+
+//favorite heart in grey box
+class FavoriteButton extends StatefulWidget {
+  const FavoriteButton({Key? key}) : super(key: key);
+
+  @override
+  _FavoriteButtonState createState() => _FavoriteButtonState();
+}
+
+class _FavoriteButtonState extends State<FavoriteButton> {
+  bool isFavorite = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Positioned(
+          bottom: 0,
+          right: 0,
+          child: Container(
+            width: 35,
+            height: 35,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+              borderRadius: const BorderRadius.only(
+                bottomRight: Radius.circular(15),
+              ),
+            ),
+            child: IconButton(
+              alignment: Alignment.center,
+              icon: Icon(
+                isFavorite ? Icons.favorite : Icons.favorite,
+                size: 20,
+                color: isFavorite ? Colors.red : Colors.white,
+              ),
+              onPressed: () {
+                setState(() {
+                  isFavorite = !isFavorite;
+                });
+                if (isFavorite == true) {
+                  print('Add to Favorite');
+                } else {
+                  print('Remove from Favorite');
+                }
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
