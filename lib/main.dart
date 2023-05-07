2049d2707d0a9edba1acfaab5b64739e738d565c
ppinng <thiti.saelee@mail.kmutt.ac.th>
2023-05-07 21:04:36 +0700

    [ADD] Sign in with google and be able to sign out

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:araigordaiwithme/screens/welcome_page/welcome_screen.dart';
+import 'firebase_options.dart';
 // import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const App());
 }
 
