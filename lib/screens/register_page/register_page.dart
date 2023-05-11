dcd8dd7a0f530de52620692577fcd63faedea770
ppinng <thiti.saelee@mail.kmutt.ac.th>
2023-05-11 15:42:39 +0700

    model user info

--- lib/screens/register_page/register_page.dart
+++ lib/screens/register_page/register_page.dart
@@ -1,5 +1,6 @@
-import 'package:araigordaiwithme/screens/Home_page/home_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 import 'package:flutter/material.dart';
@@ -178,18 +179,23 @@ class SignupScreen extends HookWidget {
                           if (formKey.currentState!.validate()) {
                             if (passwordController.text ==
                                 confirmpasswordController.text) {
-                              FirebaseAuth.instance.createUserWithEmailAndPassword(email: emailController.text, password: passwordController.text).then((value) {
+                              FirebaseAuth.instance
+                                  .createUserWithEmailAndPassword(
+                                      email: emailController.text,
+                                      password: passwordController.text)
+                                  .then((value) {
                                 print('Create account');
                                 Navigator.of(context).push(
-                                MaterialPageRoute(
-                                  builder: (context) => MoreInfo(),
-                                ),
-                              );
+                                  MaterialPageRoute(
+                                    builder: (context) => const MoreInfo(),
+                                  ),
+                                );
                               });
-
+                              
                             } else {
                               ScaffoldMessenger.of(context).showSnackBar(
                                   const SnackBar(
+                                      backgroundColor: kButtonColor,
                                       content:
                                           Text('Password does not match')));
                             }
@@ -198,13 +204,13 @@ class SignupScreen extends HookWidget {
                       ),
                     ),
                   ),
-                  
                 ],
               ),
             ],
           ),
         ),
       ),
-    );
+    ); 
   }
+
 }
