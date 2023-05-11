a96dd953a26a1ca56043aa43843c8a561a81118b
icesaran3 <icesaran3@gmail.com>
2023-05-12 04:18:30 +0700

    [FIX] Del Auth_repo and fix nav to userpage

--- lib/controller/profile_controller.dart
+++ lib/controller/profile_controller.dart
@@ -1,17 +1,16 @@
-import 'package:araigordaiwithme/user_repository/auth_repository.dart';
 import 'package:araigordaiwithme/user_repository/user_repository.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:araigordaiwithme/models/model_userinfo.dart';
 
 class ProfileController extends GetxController {
   static ProfileController get instance => Get.find();
 
-  final _authRepo = Get.put(AuthenticationRepository());
   final _userRepo = Get.put(UserRepository());
 
   getUserData() {
-    final email = _authRepo.firebaseUser.value?.email;
+    final email = FirebaseAuth.instance.currentUser!.email;
     if (email != null) {
       return _userRepo.getUserDetails(email);
     } else {
