5ecc94d4b4c1d56a4093f29ae50c8be029cd9170
BbenHhan <benyalak.hanp@kmutt.ac.th>
2023-05-15 03:01:37 +0700

    show fav food from Favorites collection

--- lib/screens/pages/favorite.dart
+++ lib/screens/pages/favorite.dart
@@ -4,65 +4,16 @@ import 'package:araigordaiwithme/screens/welcome_page/welcome_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:araigordaiwithme/constant.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-class Menu {
-  final String name;
-  final String image;
-  final int calories;
-  final double price;
-  final String location;
-
-  Menu(
-      {required this.name,
-      required this.image,
-      required this.calories,
-      required this.price,
-      required this.location});
-}
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Favorite extends StatefulWidget {
-  const Favorite({super.key});
+  const Favorite({Key? key}) : super(key: key);
 
   @override
-  State<Favorite> createState() => _FavoriteState();
+  _FavoriteState createState() => _FavoriteState();
 }
 
 class _FavoriteState extends State<Favorite> {
-  final List<Menu> menus = [
-    Menu(
-        name: 'Spicy fried chicken with basil leaves',
-        image: 'images/krapao.png',
-        calories: 250,
-        price: 10.99,
-        location: 'New York'),
-    Menu(
-        name: 'Spicy Chicken Wings',
-        image: 'images/wingsaps.png',
-        calories: 350,
-        price: 14.99,
-        location: 'Los Angeles'),
-    Menu(
-        name: 'Grilled Chicken',
-        image: 'images/kaiyang.png',
-        calories: 450,
-        price: 19.99,
-        location: 'Chicago'),
-    Menu(
-        name: 'Chicken rice ',
-        image: 'images/kaowmungai.png',
-        calories: 550,
-        price: 24.99,
-        location: 'Houston'),
-    Menu(
-        name: 'Papaya Salad',
-        image: 'images/papayasalad.png',
-        calories: 650,
-        price: 29.99,
-        location: 'Miami'),
-  ];
-
-  // FoodList({super.key});
-  bool isFavorite = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -87,7 +38,8 @@ class _FavoriteState extends State<Favorite> {
           IconButton(
             onPressed: () {
               Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => const UserPage()));
+                MaterialPageRoute(builder: (context) => const UserPage()),
+              );
             },
             icon: Image.asset("images/forgot.png"),
           ),
@@ -95,70 +47,114 @@ class _FavoriteState extends State<Favorite> {
       ),
       drawer: const DrawerBar(),
       backgroundColor: kBackgroundColor,
-      body: ListView.builder(
-        itemCount: menus.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Column(
-            children: <Widget>[
-              Container(
-                width: 348,
-                height: 100,
-                margin: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Stack(
-                  children: [
-                    Row(
-                      //Food image
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.only(left: 20, right: 20),
-                          child: Image.asset(
-                            menus[index].image,
-                            height: 75,
-                            width: 100,
-                            colorBlendMode: BlendMode.darken,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        //Food Name
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                menus[index].name,
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.black,
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('Food').snapshots(),
+        builder: (context, foodSnapshots) {
+          if (foodSnapshots.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection('Favorites')
+                  .snapshots(),
+              builder: (context, favoritesSnapshots) {
+                if (favoritesSnapshots.connectionState ==
+                    ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else {
+                  // Get a list of favorite food ids
+                  final List<int> favoriteFoodIds = favoritesSnapshots
+                      .data!.docs
+                      .map((doc) => doc['foodid'] as int)
+                      .toList();
+
+                  // Filter the food snapshots based on favorite food ids
+                  final List<QueryDocumentSnapshot> filteredFoodSnapshots =
+                      foodSnapshots.data!.docs
+                          .where(
+                              (doc) => favoriteFoodIds.contains(doc['foodid']))
+                          .toList();
+
+                  return ListView.builder(
+                    itemCount: filteredFoodSnapshots.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      var foodData = filteredFoodSnapshots[index].data()
+                          as Map<String, dynamic>;
+
+                      return Column(
+                        children: <Widget>[
+                          Container(
+                            width: 348,
+                            height: 100,
+                            margin: const EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            child: Stack(
+                              children: [
+                                Row(
+                                  //Food image
+                                  children: <Widget>[
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                          left: 20, right: 20),
+                                      child: Image.network(
+                                        foodData['image'],
+                                        height: 75,
+                                        width: 100,
+                                        colorBlendMode: BlendMode.darken,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                    //Food Name
+                                    Expanded(
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: <Widget>[
+                                          Text(
+                                            foodData['name'],
+                                            style: const TextStyle(
+                                              fontSize: 16,
+                                              color: Colors.black,
+                                            ),
+                                          ),
+                                          const SizedBox(
+                                            height: 5,
+                                          ),
+                                          //Food Calories
+                                          Text(
+                                            'Calories: ${foodData['calories']}',
+                                            style: const TextStyle(
+                                              fontSize: 16,
+                                              color: Colors.grey,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ],
                                 ),
-                              ),
-                              const SizedBox(
-                                height: 5,
-                              ),
-                              //Food Calories
-                              Text(
-                                'Calories: ${menus[index].calories}',
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                  color: Colors.grey,
-                                ),
-                              ),
-                            ],
+                                //Grey Box bottom right
+                                const FavoriteButton(),
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                    //Grey Box bottom right
-                    const FavoriteButton(),
-                  ],
-                ),
-              ),
-            ],
-          );
+                        ],
+                      );
+                    },
+                  );
+                }
+              },
+            );
+          }
         },
       ),
     );
@@ -176,8 +172,35 @@ class FavoriteButton extends StatefulWidget {
 class _FavoriteButtonState extends State<FavoriteButton> {
   bool isFavorite = false;
 
+  Future<bool> checkIfFavorite(String foodId) async {
+    // Replace 'YOUR_COLLECTION_NAME' with the actual collection name in Firebase
+    QuerySnapshot snapshot = await FirebaseFirestore.instance
+        .collection('YOUR_COLLECTION_NAME')
+        .where('foodId', isEqualTo: foodId)
+        .get();
+
+    return snapshot.docs.isNotEmpty;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final String foodId = ''; // Add the foodId here for comparison
+
+    return FutureBuilder<bool>(
+      future: checkIfFavorite(foodId),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        } else if (snapshot.hasData && snapshot.data == true) {
+          return buildFavoriteButton(true);
+        } else {
+          return buildFavoriteButton(false);
+        }
+      },
+    );
+  }
+
+  Widget buildFavoriteButton(bool isFavorite) {
     return Stack(
       children: [
         Positioned(
@@ -204,7 +227,7 @@ class _FavoriteButtonState extends State<FavoriteButton> {
                 setState(() {
                   isFavorite = !isFavorite;
                 });
-                if (isFavorite == true) {
+                if (isFavorite) {
                   print('Add to Favorite');
                 } else {
                   print('Remove from Favorite');
